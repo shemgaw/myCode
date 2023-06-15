41177f67958eb11f7408a2f6c22af9bfac26e75d
shemgaw <przemgawlik@gmail.com>
2023-06-15 11:16:41 +0200

    update 15.06.23

--- FindDuplicateNumberInArray.java
+++ FindDuplicateNumberInArray.java
@@ -1,10 +1,15 @@
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FindDuplicateNumberInArray {
     public static void main(String[] args) {
-        int[] array = new int[] {1,2,3,3,4,5,5,6};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6};
+        System.out.println("------First Solution:------");
         findDuplicates(array);
+        System.out.println("------Second Solution:------");
+        findDuplicates2(array);
+        System.out.println("------Third Solution:------");
+        findDuplicates2EvenMorePacked(array);
     }
 
     private static void findDuplicates(int[] array) {
@@ -18,13 +23,30 @@ public class FindDuplicateNumberInArray {
 
         Set<Map.Entry<Integer, Integer>> entries = countMap.entrySet();
         System.out.println("Liczby zdublowane: ");
+        boolean areAnyDoubledNumbers = false;
         for (Map.Entry<Integer, Integer> entry : entries) {
-            if (entry.getValue() > 1){
-                System.out.println(entry.getKey());
+            if (entry.getValue() > 1) {
+                areAnyDoubledNumbers = true;
+                System.out.println("[" + entry.getKey() + "]");
             }
-
         }
-
+        if (!areAnyDoubledNumbers) {
+            System.out.println("brak");
+        }
     }
 
+    private static void findDuplicates2(int[] arrayOfNumbers) {
+        List<Integer> listOfNumbers = Arrays.stream(arrayOfNumbers).boxed().toList();
+        Set<Integer> doubledNumbers = listOfNumbers.stream()
+                .filter(l -> Collections.frequency(listOfNumbers, l) > 1)
+                .collect(Collectors.toSet());
+        System.out.println(doubledNumbers.size() != 0 ? "Duplicated numbers are: " + doubledNumbers : "No duplicated numbers");
+    }
+
+    private static void findDuplicates2EvenMorePacked(int[] arrayOfNumbers) {
+        Set<Integer> doubledNumbers = Arrays.stream(arrayOfNumbers).boxed().toList().stream()
+                .filter(l -> Collections.frequency(Arrays.stream(arrayOfNumbers).boxed().toList(), l) > 1)
+                .collect(Collectors.toSet());
+        System.out.println(doubledNumbers.size() != 0 ? "Duplicated numbers are: " + doubledNumbers : "No duplicated numbers");
+    }
 }
